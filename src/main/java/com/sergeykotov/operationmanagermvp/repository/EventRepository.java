1d88bbe2f5a015589c91efe1a6aa85e713dfbb6a
Sergey Kotov <sergey1kotov@protonmail.com>
2020-05-26 12:34:09 +0200

    fix EventRepository

--- src/main/java/com/sergeykotov/operationmanagermvp/repository/EventRepository.java
+++ src/main/java/com/sergeykotov/operationmanagermvp/repository/EventRepository.java
@@ -3,15 +3,22 @@ package com.sergeykotov.operationmanagermvp.repository;
 import com.sergeykotov.operationmanagermvp.event.Event;
 import org.springframework.stereotype.Repository;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class EventRepository {
     private static final String EXTRACT_CMD = "select e.id, e.start_timestamp, e.start_time, e.end_timestamp, e.end_time, e.elapsed, e.action, e.entity, e.name, e.user, e.note from event e";
-    private static final String EXTRACT_SINCE_CMD = "select e.id, e.start_timestamp, e.start_time, e.end_timestamp, e.end_time, e.elapsed, e.action, e.entity, e.name, e.user, e.note from event e where e.start_time >= ?";
+    private static final String EXTRACT_SINCE_CMD = "select e.id, e.start_timestamp, e.start_time, e.end_timestamp, e.end_time, e.elapsed, e.action, e.entity, e.name, e.user, e.note from event e where e.start_timestamp >= ?";
     private static final String CREATE_CMD = "insert into event (start_timestamp,start_time,end_timestamp,end_time,elapsed,action,entity,name,user,note) values (?,?,?,?,?,?,?,?,?,?)";
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
 
     private List<Event> extract(ResultSet resultSet) throws SQLException {
         List<Event> events = new ArrayList<>();
@@ -19,9 +26,9 @@ public class EventRepository {
             Event event = new Event();
             event.setId(resultSet.getLong("id"));
             event.setStartTimestamp(resultSet.getLong("start_timestamp"));
-            event.setStart(resultSet.getTimestamp("start_time").toLocalDateTime());
+            event.setStart(LocalDateTime.parse(resultSet.getString("start_time"), formatter));
             event.setEndTimestamp(resultSet.getLong("end_timestamp"));
-            event.setEnd(resultSet.getTimestamp("end_time").toLocalDateTime());
+            event.setEnd(LocalDateTime.parse(resultSet.getString("end_time"), formatter));
             event.setElapsed(resultSet.getLong("elapsed"));
             event.setAction(Event.Action.valueOf(resultSet.getString("action")));
             event.setEntity(Event.Entity.valueOf(resultSet.getString("entity")));
@@ -55,9 +62,9 @@ public class EventRepository {
         try (Connection connection = ConnectionPool.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(CREATE_CMD)) {
             preparedStatement.setLong(1, event.getStartTimestamp());
-            preparedStatement.setTimestamp(2, Timestamp.valueOf(event.getStart()));
+            preparedStatement.setString(2, event.getStart().format(formatter));
             preparedStatement.setLong(3, event.getEndTimestamp());
-            preparedStatement.setTimestamp(4, Timestamp.valueOf(event.getEnd()));
+            preparedStatement.setString(4, event.getEnd().format(formatter));
             preparedStatement.setLong(5, event.getElapsed());
             preparedStatement.setString(6, event.getAction().name());
             preparedStatement.setString(7, event.getEntity().name());
