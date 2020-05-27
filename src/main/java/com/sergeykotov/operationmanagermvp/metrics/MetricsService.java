3a742c4e51e8029288910ec8e451436fce6cb690
Sergey Kotov <sergey1kotov@protonmail.com>
2020-05-27 02:10:28 +0200

    implement Metrics evaluation

--- src/main/java/com/sergeykotov/operationmanagermvp/metrics/MetricsService.java
+++ src/main/java/com/sergeykotov/operationmanagermvp/metrics/MetricsService.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sergeykotov.operationmanagermvp.exception.ExtractionException;
 import com.sergeykotov.operationmanagermvp.exception.NotFoundException;
-import com.sergeykotov.operationmanagermvp.model.Group;
-import com.sergeykotov.operationmanagermvp.model.Op;
+import com.sergeykotov.operationmanagermvp.model.*;
 import com.sergeykotov.operationmanagermvp.repository.GroupRepository;
 import com.sergeykotov.operationmanagermvp.repository.OpRepository;
 import org.slf4j.Logger;
@@ -16,8 +15,10 @@ import org.springframework.stereotype.Service;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 @Service
 public class MetricsService {
@@ -57,8 +58,31 @@ public class MetricsService {
     }
 
     private Metrics evaluate(List<Op> ops) {
+        Set<Task> tasks = ops.stream().map(Op::getTask).collect(Collectors.toSet());
+        Set<Executor> executors = ops.stream().map(Op::getExecutor).collect(Collectors.toSet());
+        Set<Period> periods = ops.stream().map(Op::getPeriod).collect(Collectors.toSet());
+        Set<Group> groups = ops.stream().map(Op::getGroup).collect(Collectors.toSet());
+
+        long start = periods.stream().mapToLong(Period::getStart).min().orElse(0L);
+        long end = periods.stream().mapToLong(Period::getEnd).max().orElse(0L);
+
+        double mean = ops.stream().mapToDouble(Op::getCost).sum() / executors.size();
+        double deviation = 0.0;
+        for (Executor executor : executors) {
+            double cost = ops.stream().filter(o -> o.getExecutor().equals(executor)).mapToDouble(Op::getCost).sum();
+            deviation += Math.abs(cost - mean);
+        }
+        deviation = deviation / executors.size();
+
         Metrics metrics = new Metrics();
         metrics.setOpCount(ops.size());
+        metrics.setTaskCount(tasks.size());
+        metrics.setExecutorCount(executors.size());
+        metrics.setPeriodCount(periods.size());
+        metrics.setGroupCount(groups.size());
+        metrics.setStart(start);
+        metrics.setEnd(end);
+        metrics.setMeanDeviation(deviation);
         return metrics;
     }
 
